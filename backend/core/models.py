2037faa597b39fd1157951d3ab02cbd80cc004b2
Nathaniel81 <nathanield356@gmail.com>
2024-05-24 22:34:06 +0300

    Introduced Listing model to structure and manage property data efficiently

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Listing(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    street_address = models.CharField(max_length=255)
+    apt_suite = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    province = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    guest_count = models.IntegerField()
+    bedroom_count = models.IntegerField()
+    bed_count = models.IntegerField()
+    bathroom_count = models.IntegerField()
+    amenities = models.JSONField(default=list)
+    listing_photo_paths = models.JSONField(default=list)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    highlight = models.CharField(max_length=255)
+    highlight_desc = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
